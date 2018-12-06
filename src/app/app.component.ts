15170687ebc370bb158ad147621c9a6a64a8051d
mohammedachbar <achbarmohammed@gmail.com>
2018-12-06 14:42:37 +0000

    tp7AngularForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tp7AngularForm';
+export class AppComponent implements OnInit {
+myForm: FormGroup;
+constructor(fb: FormBuilder) {
+
+  this.myForm = fb.group({'nom': new FormControl(), 'age': new FormControl(), 'ville': new FormControl()});
+
+}
+
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  afficherForm() {
+      alert(this.myForm);
+  }
+
 }
